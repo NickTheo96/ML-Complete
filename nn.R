cf5a8f3b84c61be16eecda5c1361a3ee54282d08
NickTheo96 <nicktheodoulou96@gmail.com>
2018-06-22 19:33:04 +0100

    *Correct algorithm to find train.Y *now only need to come up with a sort to find the shortest Euclidian distance *Could also instead use functions in code

--- nn.R
+++ nn.R
@@ -1,4 +1,4 @@
-#Need to run the setwd()code to set to a folder that has the iris data set inside it
+#Need to run the setwd()code to set to a folder that has the dataset data set inside it
 #source... may be run in a terminal to run the code
 
 # setwd("C:/Users/Nick/Google Drive/Free Time/CS3920_CS4920_CS5920 Machine Learning/Assignment 1/My work")
@@ -6,16 +6,18 @@
 # setwd("/Users/nick/R/Assignment1")
 
 #Load in the data set, no header, na strings just incase there are missing values
-iris <- read.csv("iris.txt",header=F, na.strings="?")
+dataset <- read.csv("ionosphere.txt",header=F, na.strings="?")
 
-trainlength =70#this must be specified by the user
+trainlength =200#this must be specified by the user
 #Initialise the values 
-number_of_attributes = dim(iris)[2]-1
+number_of_attributes = dim(dataset)[2]-1
 EuclidSqSum=0
 EuclidSq=0
-testlength = dim(iris)[1]-trainlength
+testlength = dim(dataset)[1]-trainlength
 K_number_n_n=3
 sumclassificationelement=0
+pluscounter=0
+minuscounter=0
 
 #Initialise the matrices
 train.X <- matrix(nrow =trainlength, ncol=number_of_attributes)
@@ -27,16 +29,16 @@ confusionmatrix <- matrix(c(0,0,0,0),nrow = 2, ncol=2)
 
 #loop creating the train.X and test.X matrices
 for(i in 1:number_of_attributes)
-{#train.X is to be a 4x70 matrix (in iris case), of the first 70 rows of the attributes the next 2 lines create this
-  train.X[,i] <- matrix(iris[1:trainlength,i])#for loop to create each column of a given attribute first
+{#train.X is to be a 4x70 matrix (in dataset case), of the first 70 rows of the attributes the next 2 lines create this
+  train.X[,i] <- matrix(dataset[1:trainlength,i])#for loop to create each column of a given attribute first
   train.X <- cbind(train.X)#this bunds each column to creat a number_of_attributes by trainlength matrix
   #test.X is also created in this 4loop which has the dimensions of (dataset-trainlength) by number of attributes
-  test.X[,i] <- matrix(iris[(trainlength+1):dim(iris)[1],i])
+  test.X[,i] <- matrix(dataset[(trainlength+1):dim(dataset)[1],i])
   test.X <- cbind(test.X)
 }
 
-train.Y <- matrix(iris[1:trainlength,number_of_attributes+1])#create the train.Y and test.Y matrices
-test.Y <- matrix(iris[(trainlength+1):dim(iris)[1],number_of_attributes+1])
+train.Y <- matrix(dataset[1:trainlength,number_of_attributes+1])#create the train.Y and test.Y matrices
+test.Y <- matrix(dataset[(trainlength+1):dim(dataset)[1],number_of_attributes+1])
 
 
 #triplenested 4 loop to find the Euclidean distance for the first test data row
@@ -61,35 +63,43 @@ for (k in 1:testlength)#sums through the test data column by column
 for (j in 1:testlength)
 {
   for(i in 1:K_number_n_n)#sums through the nearest neighbour
-  {#matrix that is ordered, i will later use my own algorithm to order it so that k nearest neighbours can be found
+  {#matrix that is ordered, i will later use my own algorithm to order it so that k nearest neighbours can be found!!!
     knnelements[i,j] <- matrix(order(EuclidianMatrix[,j])[i])
   }
 }
 
-for (j in 1:testlength)#create a 1 by testlength matrix that has the  classification of each element
-{
-  for (i in 1:K_number_n_n)
+#nested for loop that countes the number of plus and minus in the knnelement matrix for each column and then assignes a classification based on
+for(j in 1:testlength)#which ever scores higher 
   {
-    sumclassificationelement=sumclassificationelement+train.Y[knnelements[i,j]]#adds the knn columns to perform an average
-    if (sumclassificationelement>0)#as the classification ia either p/m 1 this is a simple if statement that decidesif the 
-    {#classification is either plus 1 or minus 1 after adding all the elements
-      sumclassificationelement=1  
-    }
-    else if(sumclassificationelement<0)
+    for (i in 1:K_number_n_n)
+      {
+        if(train.Y[knnelements[i,j]]==1)
+          {
+            pluscounter = pluscounter +1#counts the number of +1 classification for each nearest number
+          }
+        else if(train.Y[knnelements[i,j]]==-1)
+          {
+            minuscounter = minuscounter +1
+          }
+        else
+          {
+            print ("Please pick an odd number for k")#accounts for any errors that might arrise when an even k is chosen
+          }
+      }
+  if (minuscounter > pluscounter)#decides if the predicted.Y classification should be either plus or minus 1 using the outer loop
     {
-      sumclassificationelement=-1 
+      predicted.Y[j]=-1
     }
-    else#idealy a odd number is chosen for k such that the total is not 0 however if in an edge case an error is printed
+      else if(minuscounter < pluscounter)
     {
-      print ("There is an error, there are the same amount of +1 and -1 classifications for the nearest neighbours")
+      predicted.Y[j]=+1
     }
-  }
-  predicted.Y[j]<-matrix(sumclassificationelement)#puts all the values into a matrix
-  sumclassificationelement=0#resets the counter for each iteration 
+  minuscounter=0#resets the counter after the inner loop is performed
+  pluscounter=0
 }
 
 
-#confusion matrix {(a,b),(c,d)} where a= # predicted = -1 and test =-1, d = # predicted = 1 and test = 1, c = #predicted = 1 and test = -1
+#confusionmatrix {(a,b),(c,d)} where a= # predicted = -1 and test =-1, d = # predicted = 1 and test = 1, c = #predicted = 1 and test = -1
 #b = #predicted -1 and test = 1
 for (i in 1:testlength)
   {
@@ -99,7 +109,7 @@ for (i in 1:testlength)
           {
             confusionmatrix[1,1]=confusionmatrix[1,1]+1
           }
-        else 
+        else if(test.Y[i]==1)
           {
             confusionmatrix[1,2]=confusionmatrix[1,2]+1
           }
@@ -110,15 +120,18 @@ for (i in 1:testlength)
           {
             confusionmatrix[2,2]=confusionmatrix[2,2]+1
           }
-        else
+        else if(test.Y[i]==-1)
           {
             confusionmatrix[2,1]=confusionmatrix[2,1]+1
           }
       }
-  }
+}
 
-
-
+##########knn built into R can be used to test whether my code works
+#library(class)
+#predicted.Y <- knn(train.X,test.X,train.Y,k=3)
+#table(predicted.Y,test.Y)
+########
 
 
 
