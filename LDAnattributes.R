b48cbad9e034aa8d57f1c0a4a5acc71a6e16e36b
NickTheo96 <nicktheodoulou96@gmail.com>
2018-07-06 23:28:50 +0100

    Calculated confusion matrix however there is some kind of error as the predictions are wrong

--- LDAnattributes.R
+++ LDAnattributes.R
@@ -54,15 +54,15 @@ for(i in 1:number_of_attributes)
   mu_plus[1,i] = mu_plus[1,i]/pluscounter
 }
 
-sigma <-matrix(0,nrow =number_of_attributes, ncol=number_of_attributes)
+sigma <-matrix(0,nrow =number_of_attributes, ncol=number_of_attributes)#initialise the sigma matrix
 
-for(k in 1:trainlength)
+for(k in 1:trainlength)#triple nested for loop to find the sigma matrix
 {
   for(j in 1:number_of_attributes)
   {
     for(l in 1:number_of_attributes)
     {
-      if(trainLabels[k]==-1)
+      if(trainLabels[k]==-1)#the equation depends on the mean of the classification
       {
         sigma[l,j]=sigma[l,j]+((trainObjects[k,l]-mu_minus[1,l])*(trainObjects[k,j]-mu_minus[1,j]))
       }
@@ -73,6 +73,72 @@ for(k in 1:trainlength)
     }
   }
 }
+
+sigma <-(1/(trainlength-2))*sigma
+
+mu_minus_column <-matrix(0,nrow =number_of_attributes, ncol=1)#initialise the column matrix equivalent of the mean vector
+mu_plus_column <-matrix(0,nrow =number_of_attributes, ncol=1)
+                                                                              
+for(i in 1:number_of_attributes)
+{
+  mu_minus_column[i,1]=mu_minus[1,i]
+  mu_plus_column [i,1]=mu_minus[1,i]
+}
+
+inverseSigma =solve(sigma)#r function that finds the inverse of the matrix so that delta can be calculated 
+
+delta_matrix <- matrix(0,nrow=testlength,ncol=2)#define a 2 by testlength matrix that stores the delta values for the two
+#classifications so that they can be compared. 
+
+for(i in 1:testlength)
+{
+  delta_matrix[i,1]=(testObjects[i,1:4]%*%inverseSigma%*%mu_minus_column)-((1/2)*mu_minus%*%inverseSigma%*%mu_minus_column+log(pi_minus1))
+  delta_matrix[i,2]=(testObjects[i,1:4]%*%inverseSigma%*%mu_plus_column)-((1/2)*mu_plus%*%inverseSigma%*%mu_plus_column+log(pi_plus1))
+}
+
+for(i in 1:testlength)
+{
+  if(delta_matrix[i,1]>delta_matrix[i,2])#if the delta corresponnding to minus one is larger than the one correspinding to plus
+    #one, then the predicted class will be minus one
+  {
+    predicted.Y[i]=-1
+  }
+  if(delta_matrix[i,1]<delta_matrix[i,2])
+  {
+    predicted.Y[i]=1
+  }
+}
+
+confusionmatrix <- matrix(c(0,0,0,0),nrow = 2, ncol=2)#initialise the confusion matrix
+
+for (i in 1:testlength)#inefficient nested for loop used to create the confusion matrix
+{
+  if(predicted.Y[i]==-1)
+  {
+    if(testLabels[i]==-1)
+    {
+      confusionmatrix[1,1]=confusionmatrix[1,1]+1
+    }
+    else if(testLabels[i]==1)
+    {
+      confusionmatrix[1,2]=confusionmatrix[1,2]+1
+    }
+  }
+  else if(predicted.Y[i]==1)
+  {
+    if(testLabels[i]==1)
+    {
+      confusionmatrix[2,2]=confusionmatrix[2,2]+1
+    }
+    else if(testLabels[i]==-1)
+    {
+      confusionmatrix[2,1]=confusionmatrix[2,1]+1
+    }
+  }
+}
+
+
+
 
 
 
